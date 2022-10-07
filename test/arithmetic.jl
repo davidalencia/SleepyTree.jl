3477bd3d31b90befc00bec8fedd3fcd6c59c33ad
David Valencia <davidalencia@gmail.com>
2022-10-07 11:21:53 -0500

    :white_check_mark: substraction tests

--- test/arithmetic.jl
+++ test/arithmetic.jl
@@ -11,8 +11,8 @@
             @test string(x+y) == "$(var(1,1))+$(var(2,1))" broken=true
         end
         @testset "nvars=1, grad=2" begin
-            x, = setvariables(2,1,Int)
-            @test string(x+x) == "2$(var(2,1))" broken=true
+            x, = setvariables(1,2,Int)
+            @test string(x+x) == "2$(var(1,1))"
         end
         @testset "nvars=3, grad=4" begin 
             x,y,z = setvariables(3,4,Int)
@@ -39,6 +39,73 @@
         @testset "commutativity" begin
             x,y,z = setvariables(3,4,Int)
             @test string(10+y) == string(y+10)
+        end
+    end
+end
+
+@testset "Substraction" verbose=true begin
+    @testset "SleepyTree - SleepyTree" begin
+        @testset "nvars=1, grad=1" begin
+            x, = setvariables(1,1,Int)
+            @test string(x-x) == "0" broken=true
+            @test string(3x-x) == "2$(var(1,1))" broken=true
+            @test string(x-3x) == "-2$(var(1,1))" broken=true
+        end
+        @testset "nvars=2, grad=1" begin
+            x,y = setvariables(2,1,Int)
+            @test string(x-y) == "$(var(1,1))-$(var(2,1))" broken=true
+        end
+        @testset "nvars=1, grad=2" begin
+            x, = setvariables(1,2,Int)
+            @test string(x-x) == "0" broken=true
+        end
+        @testset "nvars=3, grad=4" begin 
+            x,y,z = setvariables(3,4,Int)
+            @test string(x-y-z) == "$(var(1,1))-$(var(2,1))-$(var(3,1))"
+            @test string(y-z) == "$(var(2,1))-$(var(3,1))"
+            @test string(z-y) == "$(var(2,1))-$(var(3,1))"
+        end
+    end
+    @testset "SleepyTree - Number" begin
+        @testset "nvars=1, grad=1" begin
+            x, = setvariables(1,1,Int)
+            @test string(x-x) == "0" broken=true
+            @test string(3x-x) == "2$(var(1,1))" broken=true
+            @test string(x-3x) == "-2$(var(1,1))" broken=true
+        end
+        @testset "nvars=2, grad=1" begin
+            x,y = setvariables(2,1,Int)
+            @test string(x-y) == "$(var(1,1))-$(var(2,1))" broken=true
+        end
+        @testset "nvars=1, grad=2" begin
+            x, = setvariables(1,2,Int)
+            @test string(x-x) == "0" broken=true
+        end
+        @testset "nvars=3, grad=4" begin 
+            x,y,z = setvariables(3,4,Int)
+            @test string(x-y-z) == "$(var(1,1))-$(var(2,1))-$(var(3,1))"
+            @test string(y-z) == "$(var(2,1))-$(var(3,1))"
+            @test string(z-y) == "$(var(2,1))-$(var(3,1))"
+        end
+    end
+    @testset "- SleepyTree" begin
+        @testset "nvars=1, grad=1" begin
+            x, = setvariables(1,1,Int)
+            @test string(-x) == "-$(var(1,1))" broken=true
+        end
+        @testset "nvars=2, grad=1" begin
+            x,y = setvariables(2,1,Int)
+            @test string(-y) == "-$(var(2,1))" broken=true
+        end
+        @testset "nvars=1, grad=2" begin
+            x, = setvariables(1,2,Int)
+            @test string(-x) == "-$(var(1,1))" broken=true
+        end
+        @testset "nvars=3, grad=4" begin 
+            x,y,z = setvariables(3,4,Int)
+            @test string(-x) == "-$(var(1,1))"
+            @test string(-y) == "-$(var(2,1))"
+            @test string(-z) == "-$(var(3,1))"
         end
     end
 end
@@ -75,5 +142,10 @@ end
         @test string((x+1)*x*y) == "$(var(1,1))$(var(2,1))+$(var(1,2))$(var(2,1))" 
         @test string((x+2)*x*y*2) == "4$(var(1,1))$(var(2,1))+2$(var(1,2))$(var(2,1))" 
         @test string((x+1)*(y+1)*y*y*y) == "$(var(2,3))+$(var(1,1))$(var(2,3))+$(var(2,4))+$(var(1,1))$(var(2,4))"
+    end
+    @testset "Sum and Sub" begin
+        x,y, = setvariables(2,4,Int)
+        @test string(x+(-y)) == "$(var(1,1))-$(var(2,1))"
+        @test string(x+(-y)-y+x) == "2$(var(1,1))-2$(var(2,1))"
     end
 end
